c4eff5aac0182a1f196cae7250509c4c759a963e
Olawale Segun <96541838+Olawalesegun@users.noreply.github.com>
2022-02-10 02:24:31 +0100

    Add files via upload

---
+++ Counter.js
@@ -0,0 +1,17 @@
+
+let newDetail = document.getElementById("number")
+//console.log(newDetail)
+
+let i = 0
+// console.log(i)
+
+function increment() {
+     i = i + 1
+     //i = i++
+     newDetail.innerHTML = i
+
+}
+
+
+
+
